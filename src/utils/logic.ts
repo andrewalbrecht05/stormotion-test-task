648839c34879f9c94ab76be5dacd7891f89ecd30
Andrii Albrekht <andrijalbrext@gmail.com>
2024-10-05 20:37:45 +0300

    fix(logic): changed the logic of computer response

--- src/utils/logic.ts
+++ src/utils/logic.ts
@@ -1,7 +1,7 @@
 const computerMove = (n: number, m: number, cur: number): number => {
     const period = n % 2 === 0 ? m + 2 : 2 * m + 2;
 
-    for (let i = m; i >= 1; i-- ) {
+    for (let i = Math.min(n,m); i >= 1; i--) {
         if (n - i === 0 || n - i === 1) {
             if ((cur + i) % 2 === 0) {
                 return i
@@ -10,13 +10,8 @@ const computerMove = (n: number, m: number, cur: number): number => {
     }
 
     for (let i = Math.min(n, m); i >= 1; i--) {
-        if (cur % 2 === 0) {
-            if ((n - i) % period == 1)
-                return i;
-        } else {
-            if ((n - i) % period == 0 || (n - i) % period == m + 1)
-                return i;
-        }
+        if ((n - i) % period == 1)
+            return i;
     }
 
     return 1;
